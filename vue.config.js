5c99f9833d17f471b85b7873690ef112639f688c
Quentin Gendre <swynfel@gmail.com>
2020-08-17 00:58:24 +0900

    Bump dependencies and project version

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@import "~@/scss/variables.scss";`
+        additionalData: `@import "~@/scss/variables.scss";`
       }
     }
   }
